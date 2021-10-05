98815038ec4f4b0c60a10698bfd28cd877615b97
AnadyaNair✔ <71205867+AnadyaNair@users.noreply.github.com>
2021-10-05 17:45:43 +0530

    Updated Rose Patterns and added a home page

---
+++ Rose Patterns/README.md
@@ -0,0 +1,3 @@
+## 🔗 - [https://anadyanair.github.io/Creative-Coding/Rose%20Patterns/](https://anadyanair.github.io/Creative-Coding/Rose%20Patterns/)
+
+Inspired by Coding Train Rose Pattern video.
--- Rose Patterns/sketch.js
+++ Rose Patterns/sketch.js
@@ -1,19 +1,22 @@
-var k = 4;
-
+var k = 5/8;
+var slider;
 function setup(){
     createCanvas(600, 600);
+    slider = createSlider(1,10,4,0.1);
 }
 
 function draw(){
+    k = slider.value();
     background("#121212");
     translate(width / 2, height / 2);
 
     beginShape();
     stroke(255);
+    noFill();
     strokeWeight(1);
 
-    for (var a = 0; a < TWO_PI; a += 0.01){
-        var r = 200 * cos(7 * a);
+    for (var a = 0; a < TWO_PI * 10; a += 0.01){
+        var r = 200 * cos(k * a);
         var x = r * cos(a);
         var y = r * sin(a);
         vertex(x,y);
